3d641a2d17da2f7c411f3c91c6c4de9b6f035de9
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-10-30 01:52:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,9 @@
     const {importAll, getScript} = await import(`https://rpgen3.github.io/mylib/export/import.mjs`);
     await Promise.all([
         'https://code.jquery.com/jquery-3.3.1.min.js',
-        'https://colxi.info/midi-parser-js/src/main.js',
-        'https://unpkg.com/@tonejs/midi' // https://github.com/Tonejs/Midi
+        'https://colxi.info/midi-parser-js/src/main.js'
     ].map(getScript));
-    const {$, MidiParser, Midi} = window;
+    const {$, MidiParser} = window;
     const rpgen3 = await importAll([
         'input'
     ].map(v => `https://rpgen3.github.io/mylib/export/${v}.mjs`));
@@ -149,21 +148,59 @@
             this.muted = false;
         }
     }
-    const outputMIDI = nodes => {
+    const outputMIDI = heap => {
         const {timeDivision} = g_midi,
-              deltaToMs = 1000 * 60 / inputBPM() / timeDivision,
-              midi = new Midi(),
-              track = midi.addTrack();
+              deltaToMs = 1000 * 60 / inputBPM() / timeDivision;
+        let track = midi.addTrack();
         for(const node of nodes) {
             const {note, velocity, start, end} = node,
                   [_start, _end] = [start, end].map(v => v * deltaToMs);
             track.addNote({
                 midi: note,
-                time : _start,
+                time: _start,
                 velocity: velocity / 0x7F,
                 duration: _start - _end
             });
         }
-        return URL.createObjectURL(new Blob(midi.toArray(), {type: 'audio/midi'}));
+        const arr = [];
+        HeaderChunks(arr);
+        TrackChunks(arr, heap);
+        return URL.createObjectURL(new Blob(new Uint8Array(arr), {type: 'audio/midi'}));
+    };
+    const to2byte = n => [(n & 0xff00) >> 8, n & 0xff],
+          to3byte = n => [(n & 0xff0000) >> 16, ...to2byte(n)];
+    const HeaderChunks = arr => {
+        arr.push(0x4D, 0x54, 0x68, 0x64); // チャンクタイプ(4byte)
+        arr.push(0x00, 0x00, 0x00, 0x06); // データ長(4byte)
+        const {formatType, tracks, timeDivision} = g_midi;
+        for(const v of [
+            formatType,
+            tracks,
+            timeDivision
+        ]) arr.push(...to2byte(v));
+    };
+    const TrackChunks = (arr, heap) => {
+        arr.push(0x4D, 0x54, 0x72, 0x6B); // チャンクタイプ(4byte)
+        arr.push(0x00, 0x00, 0x00, 0x06); // データ長(4byte)
+        arr.push(...DeltaTime(0));
+        arr.push(0xFF, 0x51, 0x03, ...to3byte(60000000 / inputBPM)); // テンポ
+        while(heap.length) {
+            const {deltaTime, note, velocity} = heap.pop();
+            arr.push(...DeltaTime(deltaTime));
+            arr.push(0x90, note, velocity);
+        }
+        arr.push(...DeltaTime(0));
+        arr.push(0xFF, 0x2F, 0x00); // トラックチャンクの終わりを示す
+    };
+    const DeltaTime = n => { // 可変長数値表現
+        if(n === 0) return [0];
+        const arr = [];
+        let i = 0;
+        while(n) {
+            const _7bit = n & 0x7F;
+            n >>= 7;
+            arr.push(_7bit | (i++ ? 0x80 : 0));
+        }
+        return arr;
     };
 })();
